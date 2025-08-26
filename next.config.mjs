07080505c496094e8768eb9b6e76d4fd9b07c0a8
Jacob Diehl <jakediehl17@gmail.com>
2025-08-26 09:19:00 -0600

    edits to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// next.config.mjs
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Ensure @ resolves to /src in ALL environments (Vercel included)
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    return config;
+  },
+};
 
 export default nextConfig;
